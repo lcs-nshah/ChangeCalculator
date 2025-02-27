a6356dd8ebf9a0c675ac61ca00ef4bde8ec66d66
Neil Shah <nshah@lcs.on.ca>
2025-02-27 09:54:37 -0500

    Created an item view file for Change to display a history of results.

--- ChangeCalculator/Views/ChangeItemView.swift
+++ ChangeCalculator/Views/ChangeItemView.swift
@@ -5,3 +5,25 @@
 //  Created by Neil Shah on 2025-02-27.
 //
 
+import SwiftUI
+
+struct ChangeItemView: View {
+    
+    // MARK: Stored Properties
+    let change: Change
+    
+    // MARK: Computed Properties
+    var body: some View {
+        HStack {
+            
+            
+        }
+        .lineLimit(1)
+        .minimumScaleFactor(0.5)
+    }
+}
+
+#Preview {
+    ChangeItemView(change: Change(amountInCents: 250))
+        .padding()
+}
