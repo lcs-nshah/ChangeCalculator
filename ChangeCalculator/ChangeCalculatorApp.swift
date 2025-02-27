414ec84a117a1b87a4be2026bb2ad1627a8aa4d5
Neil Shah <nshah@lcs.on.ca>
2025-02-27 09:41:53 -0500

    Initial Commit

---
+++ ChangeCalculator/ChangeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeCalculatorApp.swift
+//  ChangeCalculator
+//
+//  Created by Neil Shah on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct ChangeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
